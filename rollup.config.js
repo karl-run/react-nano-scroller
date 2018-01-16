e29cd7f05c012ac9e8bfb4bca45d39426cb75ca5
Karl J. Overå <karloveraa@gmail.com>
2018-01-16 21:20:06 +0100

    Update packaging to include umd, cjs and umd. Fix dependencies (reducing bundled size from 11 to 2kb..)

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,73 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
+import postcssModules from 'postcss-modules';
 
-let pkg = require('./package.json');
-let external = Object.keys(pkg.dependencies);
+import pkg from './package.json';
 
-let plugins = [
+const cssExportMap = {};
+
+const commonPlugins = [
   postcss({
-    extensions: ['.css', '.sss']
+    extensions: ['.css', '.sss', '.scss'],
+    plugins: [
+      postcssModules({
+        getJSON(id, exportTokens) {
+          cssExportMap[id] = exportTokens;
+        }
+      })
+    ],
+    getExportNamed: false, //Default false, when set to true it will also named export alongside default export your class names
+    getExport(id) {
+      return cssExportMap[id];
+    }
   }),
-  babel(babelrc())
+  babel(babelrc()),
 ];
 
-export default {
-  entry: 'lib/index.js',
-  plugins: plugins,
-  external: external,
-  globals: {
-    react: 'React'
+const external = [
+  'react',
+  'react-dom',
+];
+
+export default [{
+    // browser-friendly UMD build
+    input: 'lib/index.js',
+    external: external,
+    output: {
+      file: pkg.browser,
+      format: 'umd'
+    },
+    globals: {
+      react: 'React'
+    },
+    name: 'reactNanoSpinner',
+    plugins: [
+      ...commonPlugins,
+      resolve(), // so Rollup can find `ms`
+      commonjs() // so Rollup can convert `ms` to an ES module
+    ]
   },
-  targets: [
-    {
-      dest: pkg.main,
-      format: 'umd',
-      moduleName: 'reactNanoSpinner',
-      sourceMap: true
-    }
-  ]
-};
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // the `targets` option which can specify `file` and `format`)
+  {
+    input: 'lib/index.js',
+    external: external,
+    plugins: commonPlugins,
+    output: [{
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ]
+  }
+];
