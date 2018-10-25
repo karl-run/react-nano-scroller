e03aec54ef5a7c2bc6db5eb7b9dd3c24fabdcbb7
Karl J. Overå <k@rl.run>
2018-10-25 21:58:44 +0200

    Upgrade to babel 7

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,15 @@
 import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup';
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
-import postcssModules from 'postcss-modules';
 
 import pkg from './package.json';
 
-const cssExportMap = {};
-
 const commonPlugins = [
   postcss({
-    extensions: ['.css', '.sss', '.scss'],
-    plugins: [
-      postcssModules({
-        getJSON(id, exportTokens) {
-          cssExportMap[id] = exportTokens;
-        }
-      })
-    ],
-    getExportNamed: false, //Default false, when set to true it will also named export alongside default export your class names
-    getExport(id) {
-      return cssExportMap[id];
-    }
+    extensions: ['.css'],
+    minimize: true,
   }),
-  babel(babelrc()),
+  babel(),
 ];
 
 const external = [
@@ -32,33 +17,27 @@ const external = [
   'react-dom',
 ];
 
+const input = 'lib/index.js'
+
 export default [{
-    // browser-friendly UMD build
-    input: 'lib/index.js',
-    external: external,
+    input,
+    external,
     output: {
+      name: 'reactNanoSpinner',
       file: pkg.browser,
-      format: 'umd'
+      format: 'umd',
+      globals: {
+        react: 'React'
+      },
     },
-    globals: {
-      react: 'React'
-    },
-    name: 'reactNanoSpinner',
     plugins: [
       ...commonPlugins,
-      resolve(), // so Rollup can find `ms`
-      commonjs() // so Rollup can convert `ms` to an ES module
+      commonjs()
     ]
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // the `targets` option which can specify `file` and `format`)
   {
-    input: 'lib/index.js',
-    external: external,
+    input,
+    external,
     plugins: commonPlugins,
     output: [{
         file: pkg.main,
